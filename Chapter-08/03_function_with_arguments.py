29bac690baa7a93d64bd8aa480383b0f723aef31
Omkar_Pinjarkar <omkarpinjarkar2004@gmail.com>
2026-08-28 12:34:24 +0530

    Chapter -08 Done

---
+++ Chapter-08/03_function_with_arguments.py
@@ -0,0 +1,8 @@
+def goodDay(name, ending):
+    print("Good Day, " + name)
+    print(ending)
+    return "ok"
+
+a = goodDay("Harry", "Thank you") 
+print(a)
+
---
+++ Chapter-08/Problem/08_problem_01.py
@@ -0,0 +1,9 @@
+def findMax(a,b,c):
+   return max(a,b,c)
+
+a = int(input("Enter Value Of A :"))
+b = int(input("Enter Value Of B :"))
+c = int(input("Enter Value Of C :"))
+
+result = findMax(a,b,c)
+print(f"The Maximum Value is :{result}")
